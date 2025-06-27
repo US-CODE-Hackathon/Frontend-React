67b0ac09ec999536330374ea3383a19ad214c268
gmlwlsdl <heejin094@naver.com>
2025-06-28 01:19:18 +0900

    feat: 가족 메인 페이지 라우팅 (#6)

--- src/allFiles.ts
+++ src/allFiles.ts
@@ -1,6 +1,23 @@
-import KioskMain from "./kiosk/pages/Main";
-import KioskSplash from "./kiosk/pages/Splash"
-import KioskHeader from "./kiosk/components/Header"
-import KioskButton from "./kiosk/components/Button";
+import KioskMain from './kiosk/pages/Main';
+import KioskSplash from './kiosk/pages/Splash';
+import KioskHeader from './kiosk/components/Header';
+import KioskButton from './kiosk/components/Button';
 
-export { KioskMain, KioskSplash, KioskHeader, KioskButton };
+import FamilyMain from './family/main/pages';
+import AINotification from './family/main/components/AiNotification';
+import BiographySection from './family/main/components/BiographySection';
+import EmotionChart from './family/main/components/EmotionChart';
+import StatusSummary from './family/main/components/StatusSummary';
+
+export {
+  KioskMain,
+  KioskSplash,
+  KioskHeader,
+  KioskButton,
+  // Family components
+  FamilyMain,
+  AINotification,
+  BiographySection,
+  EmotionChart,
+  StatusSummary,
+};
