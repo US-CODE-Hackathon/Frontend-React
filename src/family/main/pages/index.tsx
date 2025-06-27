5db7e7462e2cd0d952455ca79a70d4da9cd59839
gmlwlsdl <heejin094@naver.com>
2025-06-28 03:56:46 +0900

    style: 가족 메인 페이지 내부에 하단바 컴포넌트 적용

--- src/family/main/pages/index.tsx
+++ src/family/main/pages/index.tsx
@@ -2,12 +2,9 @@ import * as S from './style';
 import * as C from '@/allFiles';
 
 import { useState } from 'react';
-import { GrAnalytics } from 'react-icons/gr';
-import { IoFileTrayFull } from 'react-icons/io5';
 
 const FamilyMain = () => {
   const [activeTab, setActiveTab] = useState<'weekly' | 'monthly'>('weekly');
-  const [activeNavTab, setActiveNavTab] = useState<'analysis' | 'record'>('analysis');
 
   const notifications = [{ id: '1', message: '3일 연속 피로감 호소 • 1시간 전' }];
 
@@ -36,24 +33,7 @@ const FamilyMain = () => {
           <C.BiographySection progress={biographyProgress} />
         </S.MainContent>
 
-        <S.BottomNavigation>
-          <S.NavItem
-            active={activeNavTab === 'analysis'}
-            onClick={() => setActiveNavTab('analysis')}
-          >
-            <S.NavIcon>
-              <GrAnalytics size={24} />
-            </S.NavIcon>
-            <S.NavLabel>분석</S.NavLabel>
-          </S.NavItem>
-
-          <S.NavItem active={activeNavTab === 'record'} onClick={() => setActiveNavTab('record')}>
-            <S.NavIcon>
-              <IoFileTrayFull size={24} />
-            </S.NavIcon>
-            <S.NavLabel>기록</S.NavLabel>
-          </S.NavItem>
-        </S.BottomNavigation>
+        <C.FamilyBottomNavigation />
       </S.AppContainer>
     </S.MainContainer>
   );
