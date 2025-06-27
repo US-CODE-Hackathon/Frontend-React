b957bf39511a524e1845c48b94f57b06bfa112c2
gmlwlsdl <heejin094@naver.com>
2025-06-28 01:18:29 +0900

    style: 헤더 컴포넌트 CSS 수정

--- src/kiosk/components/Header/style.ts
+++ src/kiosk/components/Header/style.ts
@@ -1,10 +1,25 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
-  width: 100%;
+  width: 374px;
   height: 3rem;
-  border-bottom: 1px solid #F0F2F5;
+  border-bottom: 1px solid #f0f2f5;
+  background-color: #ffffff;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: fixed;
+
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
+
+  @media (max-width: 375px) {
+    height: 2.75rem;
+  }
+
+  @media (max-height: 600px) {
+    height: 2.5rem;
+  }
 `;
