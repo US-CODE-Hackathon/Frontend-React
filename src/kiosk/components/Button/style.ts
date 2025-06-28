feab6f65ac3b7d0d9a5116cce95ec6470337ac5a
gmlwlsdl <heejin094@naver.com>
2025-06-28 20:07:07 +0900

    style: 시작하기 버튼 글씨 크가 조정

--- src/kiosk/components/Button/style.ts
+++ src/kiosk/components/Button/style.ts
@@ -1,15 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const BtnContainer = styled.button`
   width: 100%;
   padding: 20px;
   border-radius: 8px;
-  background-color: #00AA17;
+  background-color: #00aa17;
   border: none;
   color: #fff;
-  transition: background-color 0.15s ease-in ;
+  transition: background-color 0.15s ease-in;
+  font-size: 24px;
+  font-weight: 600;
 
   &:hover {
-    background-color:rgb(0, 155, 21);
+    background-color: rgb(0, 155, 21);
   }
-`
+`;
